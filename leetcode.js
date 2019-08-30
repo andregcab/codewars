4e949fcc1073f0cb595bafd3e65dc2b5325ac50e
Andre Cabrera <cabrerandre@gmail.com>
2019-08-29 20:15:12 -0400

    added leetcode file

---
+++ leetcode.js
@@ -0,0 +1,23 @@
+//Two Sum
+
+/*
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function(nums, target) {
+  let indexes = []
+  
+  for(let i=0; i < nums.length; i++){
+      let currentThing = nums[i]
+      indexes.push(i)
+      for(let x=0; x < nums.length; x++){
+          if(currentThing + nums[x] === target && i !== x){
+              indexes.push(x)
+              return indexes
+          }
+      }
+      indexes.pop()
+  }
+};
