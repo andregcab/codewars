b6ed5cd3257f1fa2e4767b0c98ad96e68386c5b0
Andre Cabrera <cabrerandre@gmail.com>
2019-10-05 17:11:03 -0400

    completed challenge

--- codewars.js
+++ codewars.js
@@ -323,3 +323,12 @@ var summation = function (num) {
   }
   return sum
  }
+
+ var summation = function (num) {
+  let sum = 0
+  for (let i = 0; i <= num; i++) {
+    sum += i
+  }
+  return sum
+ }
+
