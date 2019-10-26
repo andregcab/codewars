72b51aba9e42f183e55577bb868dda3825b313a0
Andre Cabrera <cabrerandre@gmail.com>
2019-10-25 22:17:42 -0400

    deleted challenge

--- leetcode.js
+++ leetcode.js
@@ -21,25 +21,3 @@ var twoSum = function(nums, target) {
     indexes.pop();
   }
 };
-
-//Who Likes It
-
-function likes(names) {
-  switch (names.length) {
-    case 0:
-      return "no one likes this";
-      break;
-    case 1:
-      return `${names[0]} likes this`;
-      break;
-    case 2:
-      return `${names[0]} and ${names[1]} like this`;
-      break;
-    case 3:
-      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-      break;
-    default:
-      return `${names[0]}, ${names[1]} and ${names.length -
-        2} others like this`;
-  }
-}
