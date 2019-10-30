624d0a4b2cd64f6ad81b78bf45fbdf9c8d0db18a
Andre Cabrera <cabrerandre@gmail.com>
2019-10-29 22:20:23 -0400

    improved solution

--- codewars.js
+++ codewars.js
@@ -424,3 +424,12 @@ var ownedCatAndDog = function(catYears, dogYears) {
 
   return ownedYears;
 };
+
+// Better solution
+
+var ownedCatAndDog = function(cat, dog) {
+  return [
+    cat < 15 ? 0 : cat < 24 ? 1 : ((cat - 16) / 4) | 0,
+    dog < 15 ? 0 : dog < 24 ? 1 : ((dog - 14) / 5) | 0
+  ];
+};
