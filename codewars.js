201848ba8a87f872f0c47330d376f6f52871ac26
Andre Cabrera <cabrerandre@gmail.com>
2019-11-11 22:35:50 -0500

    completed challenge:

--- codewars.js
+++ codewars.js
@@ -490,3 +490,19 @@ const howMuchILoveYou = n =>
   ["not at all", "I love you", "a little", "a lot", "passionately", "madly"][
     n % 6
   ];
+
+//Calculate BMI
+
+function bmi(weight, height) {
+  let bmi = weight / Math.pow(height, 2);
+
+  if (bmi <= 18.5) {
+    return "Underweight";
+  } else if (bmi <= 25) {
+    return "Normal";
+  } else if (bmi <= 30) {
+    return "Overweight";
+  } else {
+    return "Obese";
+  }
+}
