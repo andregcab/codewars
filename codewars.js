ac4fd43e96018b0eea996b4c2abc82205b8e0cd1
Andre Cabrera <cabrerandre@gmail.com>
2020-01-14 14:49:08 -0500

    completed challenge

--- codewars.js
+++ codewars.js
@@ -1056,3 +1056,15 @@ function seven(m) {
   }
   return final;
 }
+
+//Count the divisors of a number
+
+function getDivisorsCnt(n) {
+  let count = 0;
+  for (let i = 0; i <= n; i++) {
+    if (n % i === 0) {
+      count++;
+    }
+  }
+  return count;
+}
